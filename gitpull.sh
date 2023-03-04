3244055a6d2550eb2bcd2dd8349d376693828857
E <111407784+dng-sitrep@users.noreply.github.com>
2023-03-03 22:29:55 -0500

    chmod +X

--- gitpull.sh
+++ gitpull.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo apt-get update
 git pull
--- update_upgrade_clean.sh
+++ update_upgrade_clean.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
  git pull && sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove -y && docker image prune -a
