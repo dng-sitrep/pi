9cd072711e6285e70d1c6f86006424a47567a101
e <db5f5447-10da-480f-9ca0-889771e44e43@slmail.me>
2023-05-28 04:30:30 -0400

    unboun.sh upadte

--- scripts/install_unbound.sh
+++ scripts/install_unbound.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install unbound -y
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
-rsync -ahP pi-hole.conf /etc/unbound/unbound.conf.d/
+sudo rsync -ahP pi-hole.conf /etc/unbound/unbound.conf.d/
 sudo service unbound restart
 dig pi-hole.net @127.0.0.1 -p 5335
