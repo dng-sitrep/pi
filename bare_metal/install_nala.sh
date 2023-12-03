7c9dff48dcdb9035e92fde83f890ce45aebd5429
e <db5f5447-10da-480f-9ca0-889771e44e43@slmail.me>
2023-12-03 15:25:48 -0500

    remaned files and added nlal

---
+++ bare_metal/install_nala.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#add repo
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
+
+#update and install
+sudo apt update && sudo apt install nala
